c5a2ff1ead13b070d2aefc8fcc227577a1d7df49
Muhammad <mhdibnhameed@gmail.com>
2026-02-07 15:33:59 +0530

    removed font weight of 600

--- app/font.jsx
+++ app/font.jsx
@@ -2,6 +2,6 @@ import { Roboto } from "next/font/google";
 
 export const roboto = Roboto({
     variable: "--font-roboto",
-    weight: ["400", "500", "600", "700"],
+    weight: ["400", "500", "700"],
     subsets: ["latin"],
 })
